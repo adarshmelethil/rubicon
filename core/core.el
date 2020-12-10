72770c93575c9d5c0946c6cb2a4525cac6440855
abdulbahajaj <abdulbahajaj@gmail.com>
2020-12-10 00:34:17 -0700

    Fixed problem with git gutter's fringe modified indicator

--- core/core.el
+++ core/core.el
@@ -36,7 +36,7 @@
 
 (define-fringe-bitmap 'git-gutter-fr:added [224]
       nil nil '(center repeated))
-(define-fringe-bitmap 'git-gutter-fr:added [224]
+(define-fringe-bitmap 'git-gutter-fr:modified [224]
       nil nil '(center repeated))
 (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240]
       nil nil 'bottom)
